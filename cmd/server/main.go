67c54575cebaeff64d8b4b8181389bef02ead582
Ertekes Simon <ertekessimon@gmail.com>
2026-04-18 22:59:03 +0200

    Add RabbitMQ channel handling and message publishing functionality

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -7,6 +7,8 @@ import (
 	"syscall"
 
 	amqp "github.com/rabbitmq/amqp091-go"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/pubsub"
+	"github.com/bootdotdev/learn-pub-sub-starter/internal/routing"
 )
 
 func main() {
@@ -19,7 +21,34 @@ func main() {
 	}
 	defer conn.Close()
 
+	ch, err := conn.Channel()
+	if err != nil {
+		fmt.Printf("Failed to open RabbitMQ channel: %v\n", err)
+		os.Exit(1)
+	}
+	defer ch.Close()
+
 	fmt.Println("Connected to RabbitMQ successfully")
+
+	if err := ch.ExchangeDeclare(
+		routing.ExchangePerilDirect,
+		"direct",
+		false,
+		false,
+		false,
+		false,
+		nil,
+	); err != nil {
+		fmt.Printf("Failed to declare exchange: %v\n", err)
+		os.Exit(1)
+	}
+
+	if err := pubsub.PublishJSON(ch, routing.ExchangePerilDirect, routing.PauseKey, routing.PlayingState{IsPaused: true}); err != nil {
+		fmt.Printf("Failed to publish pause message: %v\n", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Published pause message to exchange")
 
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
